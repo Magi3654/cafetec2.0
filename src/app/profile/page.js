fcab3f8299fe34dde6d165844d548c634d0c41f3
Magi3654 <magi3654@gmail.com>
2024-05-13 00:14:46 -0700

    profile editing logic

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,8 +1,10 @@
 'use client'
 import Image from "next/image";
 import { useState } from "react";
+import { redirect } from "next/navigation";
+import{useSession} from "next-auth/react";
 export default function ProfilePage(){
-    /*
+    
     const session = useSession();
     const[userName, setUserName]= useState(session.data.user.name||'')
     const {status} = session;
@@ -11,7 +13,7 @@ export default function ProfilePage(){
         e.preventDefault();
         const response = await fetch('/api/profile',{
             method: 'POST',
-            headers: {'Content-Type: 'application/json'},
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringfy({name:userName}),
         });
     }
@@ -24,7 +26,7 @@ export default function ProfilePage(){
     } 
     const userImage = session.data.user.image;
 
-*/
+
     return(
         <section className="mt-8">
             <h1 className="text-center text-yellow text-4xl mb-4">
@@ -34,13 +36,13 @@ export default function ProfilePage(){
                 <div className="flex gap-4 items-center">
                     <div>
                         <div className=" p-2 rounded-lg relative">
-                            <Image className="rounded-lg w-full h-full" src={'/logo.png'} width={250} height={250} alt={'avatar'}/>
+                            <Image className="rounded-lg w-full h-full" src={userImage} width={250} height={250} alt={'avatar'}/>
                             <button type="button">Editar Perfil</button>
                         </div>
                     </div>
                     <form className="grow" onSubmit={'handleProfileInfoUpdate'}>
                         <input type="text" placeholder="Nombre Completo" 
-                        value={'userName'} onChange={e=> setUserName(e.target.value)}/>
+                        value={userName} onChange={e=> setUserName(e.target.value)}/>
                         <input type="email"  disabled={true} value={"session.data.user.email"}></input>
                         <button type="submit">Guardar cambios</button>
                     </form>
