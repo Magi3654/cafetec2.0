5e5fdcfbf769ada710dfe923b87ec43e9f5f60be
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-12 23:54:04 -0700

    conect register

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 export async function POST(req) {
     const body = await req.json();
     mongoose.connect(process.env.MONGO_URL);
+    console.log("Coneccion");
     const createdUser = await User.create(body)
     return Response.json(createdUser);
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
-const { Schema, model, models } = require("mongoose");
+import bcrypt from 'bcrypt'
+import { model, models, Schema } from 'mongoose';
 
 const UserSchema = new Schema({
     email: {type: String, required: true, unique: true},
@@ -8,9 +9,16 @@ const UserSchema = new Schema({
         validate: pass => {
             if (!pass?.length || pass.length < 5) {
                 new Error('password must be at least 5 characters');
+                return false;
             }
         },
     },
 }, {timestamps: true});
+
+UserSchema.post('validate', function (user) {
+    const notHashedPassword = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(notHashedPassword, salt);
+})
 
 export const User = models?.User || model('User', UserSchema);
