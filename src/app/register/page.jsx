64342fac79dfabb88065808ac868a4fc1fa14764
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-12 21:53:07 -0700

    Module fixed

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,14 +1,34 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleFormSubmit(ev) {
+        ev.preventDefault();
+        fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'},
+        });
+    }
+
     return (
         <section className="mt-8">
             <h1 className="text-center text-2xl font-bold my-2">Register</h1>
-            <form className="flex flex-col mx-20">
-                <input type="email" placeholder="email" className="rounded-md bg-gray py-2 px-4 m-2"></input>
-                <input type="password" placeholder="password" className="rounded-md bg-gray py-2 px-4 m-2"></input>
+            <form className="flex flex-col mx-20" onSubmit={handleFormSubmit}>
+
+                <input type="email" placeholder="email" className="rounded-md bg-gray py-2 px-4 m-2" value={email} onChange={ev => setEmail(ev.target.value)}></input>
+
+                <input type="password" placeholder="password" className="rounded-md bg-gray py-2 px-4 m-2" value={password} onChange={ev => setPassword(ev.target.value)}></input>
+
                 <button type="submit" className="m-2 py-2 px-4 rounded-md bg-yellow text-white font-semibold text-xl">Register</button>
+                
                 <div className="my-4 text-center text-darkGray">or login with provider</div>
+                
                 <button className="flex gap-4 justify-center m-2 py-2 px-4 rounded-md border border-darkGray font-semibold text-lg">
                     <Image src={'/google.png'} alt={''} width={24} height={24} className="pt-1"></Image>
                     Login with Google
