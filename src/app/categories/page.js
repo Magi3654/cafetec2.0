447e1f951dcf2d26c2470e39b0dc230ac44c4d5d
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-14 11:22:17 -0700

    function handleNewCategorySubmit page.js

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -2,9 +2,36 @@
 import { useEffect, useState } from "react";
 import UserTabs from "../../components/layout/UserTabs";
 import { UseProfile } from "../../components/UseProfile";
+import toast from "react-hot-toast";
 
 export default function CategoriesPage(){
+    const [newCategoryName, setNewCategoryName] = useState('')
     const {loading:profileLoading, data:profileData} = UseProfile();
+
+    async function handleNewCategorySubmit(ev) {
+        ev.preventDefault();
+        const creationPromise = new Promise(async (resolve, reject) => {
+            const response = await fetch('/api/categories', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({name:newCategoryName}),
+            });
+
+            if (response.ok) {
+                resolve()
+            }
+
+            else {
+                reject()
+            }
+        });
+
+        await toast.promise(creationPromise, {
+            loading: 'Creating your new category...',
+            success: 'Category created',
+            error: 'Error, sorry...'
+        });
+    }
 
     if (profileLoading) {
         return 'Loading user info...'
@@ -17,7 +44,18 @@ export default function CategoriesPage(){
     return(
         <section className="mt-8 max-w-lg mx-auto">
             <UserTabs isAdmin={true}/>
-            categories
+            <form className="mt-8" onSubmit={handleNewCategorySubmit}>
+                <div className="flex gap-2 items-end">
+                    <div className="grow">
+                        <label>New Category Name</label>
+                        <input type="text" value={newCategoryName} onChange={ev => setNewCategoryName(ev.target.value)}></input>
+                    </div>
+
+                    <div className="pb-2">
+                        <button className="border bg-yellow" type="submit">Create</button>
+                    </div>
+                </div>
+            </form>
         </section>
     )
 }
