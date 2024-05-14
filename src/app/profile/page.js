4174338c5ec1f011a8b5cd4710aee82a7cb161d2
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-13 17:37:42 -0700

    update form profile

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,7 +2,9 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
-import{useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { resolve } from "path";
+import { rejects } from "assert";
 
 export default function ProfilePage(){
     
@@ -10,9 +12,6 @@ export default function ProfilePage(){
     const [userName, setUserName]= useState('')
     const [image, setImage] = useState('');
     const [phone, setPhone] = useState('');
-    const [streetAddress, setStreetAddress] = useState('');
-    const [postalCode, setpostalCode] = useState('');
-    const [city, setCity] = useState('');
     const [country, setCountry] = useState('');
     const {status} = session;
 
@@ -24,31 +23,76 @@ export default function ProfilePage(){
             fetch('/api/profile').then(response => {
                 response.json().then(data => {
                     setPhone(data.phone);
-                    setStreetAddress(data.streetAddress);
-                    setpostalCode(data.postalCode);
-                    setCity(data.city);
                     setCountry(data.country);
                 })
             });
         }
     }, [session, status]);
 
-    async function handleProfileInfoUpdate(e){
-        e.preventDefault();
-        const response = await fetch('/api/profile',{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringfy({name:userName}),
+    async function handleProfileInfoUpdate(ev){
+        ev.preventDefault();
+        const savingPromise = new Promise(async (resolve, reject) => {
+
+            const response = await fetch('/api/profile', {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name:userName,
+                    image,
+                    phone,
+                    country,
+                }),
+            });
+
+            if (response.ok) {
+                resolve()
+            }
+
+            else {
+                reject()
+            }
         });
+
+        await toast.promise(savingPromise, {
+            loading: 'Saving...',
+            success: 'Profile saved!',
+            error: 'Error',
+        });
+
+    }
+
+    async function handleFileChange(ev) {
+        const files = ev.target.files;
+        if (files?.length === 1) {
+            const data = new FormData;
+            data.set('file', files[0]);
+
+            const uploadPromise = fetch('/api/upload', {
+                method: 'POST',
+                body: data,
+            }).then(response => {
+                if (response.ok) {
+                    return response.json().then(link => {
+                        setImage(link);
+                    })
+                }
+                throw new Error('Something went wrong');
+            });
+
+            await toast.promise(uploadPromise, {
+                uploading: 'Uploading...',
+                success: 'Upload complete',
+                error: 'Upload error',
+            })
+        }
     }
 
     if (status === 'loading'){
         return 'Cargando...';
     }
-    if (status === 'unauthnticated'){
+    if (status === 'unauthenticated'){
         return redirect ('/login');
-    } 
-    const userImage = session.data.user.image;
+    }
 
 
     return(
@@ -59,46 +103,42 @@ export default function ProfilePage(){
             <div className="max-w-md mx-auto ">
                 <div className="flex gap-4 items-center">
                     <div>
-                        <div className=" p-2 rounded-lg relative">
-                            <Image className="rounded-lg w-full h-full" src={userImage} width={250} height={250} alt={'avatar'}/>
-                            <button type="button">Editar Perfil</button>
+                        <div className=" p-2 rounded-lg relative max-w-[120px]">
+                            {image && (
+                                <Image className="rounded-lg w-full h-full" src={image} width={250} height={250} alt={'avatar'}/>
+                            )}
+                            <label>
+                                <input type="file" className="hidden" onChange={handleFileChange}></input>
+                                <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">Edit</span>
+                            </label>
                         </div>
                     </div>
 
-                    <form className="grow" onSubmit={handleProfileInfoUpdate}>
-                        <label className="my-2">Nombre Completo</label>
-                        <input type="text" placeholder="Nombre Completo" 
-                            value={userName} onChange={ev => setUserName(ev.target.value)}/>
-
-                        <label className="my-2">Email</label>
-                        <input type="email" disabled={true}
-                            value={session.data.user.email} placeholder="email"></input>
-
-                        <label className="my-2">Phone Number</label>
-                        <input type="tel" disabled={true}
-                            value={phone} placeholder="Phone Number" onChange={ev => setPhone(ev.target.value)}></input>
-
-                        <label className="my-2">Street Address</label>
-                        <input type="text" disabled={true}
-                            value={streetAddress} placeholder="Street Address" onChange={ev => setStreetAddress(ev.target.value)}></input>
-                            
-                        <div className="flex gap-2">
-                            <div>
-                                <label className="my-2">Postal Code</label>
-                                <input type="text" disabled={true}
-                                    value={postalCode} placeholder="Postal Code" onChange={ev => setpostalCode(ev.target.value)}></input>
-                            </div>
-
-                            <div>
-                                <label className="my-2">City</label>
-                                <input type="text" disabled={true}
-                                    value={city} placeholder="City" onChange={ev => setCity(ev.target.value)}></input>
-                            </div>
+                    <form className="grow flex flex-col" onSubmit={handleProfileInfoUpdate}>
+                        <div className="flex flex-col m-1">
+                            <label className="my-2 font-semibold text-sm">Nombre Completo</label>
+                            <input type="text" placeholder="Nombre Completo" className="rounded-md bg-gray py-2 px-4 my-2"
+                                value={userName} onChange={ev => setUserName(ev.target.value)}/>
                         </div>
 
-                        <label className="my-2">Country</label>
-                        <input type="text" disabled={true}
-                            value={country} placeholder="Country" onChange={ev => setCountry(ev.target.value)}></input>
+                        <div className="flex flex-col m-1">
+                            <label className="my-2 mr-3 font-semibold">Email</label>
+                            <input type="email" disabled={true} className="rounded-md bg-gray py-2 px-4 my-2"
+                                value={session.data.user.email} placeholder="email"></input>
+                        </div>
+
+                        <div className="flex flex-col m-1">
+
+                            <label className="my-2 mr-3 font-semibold">Phone Number</label>
+                            <input type="tel" disabled={true} className="rounded-md bg-gray py-2 px-4 my-2"
+                                value={phone} placeholder="Phone Number" onChange={ev => setPhone(ev.target.value)}></input>
+                        </div>
+
+                        <div className="flex flex-col m-1">
+                            <label className="my-2 mr-3 font-semibold">Country</label>
+                            <input type="text" disabled={true} className="rounded-md bg-gray py-2 px-4 my-2"
+                                value={country} placeholder="Country" onChange={ev => setCountry(ev.target.value)}></input>
+                        </div>
 
                         <button type="submit">Guardar cambios</button>
                     </form>
--- src/models/User.js
+++ src/models/User.js
@@ -2,19 +2,12 @@ import bcrypt from 'bcrypt'
 import { model, models, Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-    //name: {type: String},
+    name: {type: String},
     email: {type: String, required: true, unique: true},
-    /*password: {type: String},
-    image: {type:String},
-    */
-    password: {
-        type: String,
-        
-    },
-    /*streetAddress: {type: String},
-    postalCode: {type: String},
-    city: {type: String},
-    country: {type: String},*/
+    password: {type: String},
+    image: {type: String},
+    phone: {type: String},
+    country: {type: String},
 }, {timestamps: true});
 
 UserSchema.post('validate', function (user) {
