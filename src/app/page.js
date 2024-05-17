74b184860c1f642c4a4b8e593c4a134d0ccf0fd7
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-17 12:31:27 -0700

    menu page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Header from "@/components/layout/Header";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 import Hero from "@/components/layout/Hero";
 import TopMenu from "@/components/layout/TopMenu";
 
@@ -7,6 +8,21 @@ export default function Home() {
     <>
     <Hero/>
     <TopMenu/>
+    <section className="text-center my-16" id="about">
+      <SectionHeaders subHeader={'Nuestra historia'} mainHeader={'Sobre nosotros'} />
+      <div className="max-w-md mx-auto mt-4 flex flex-col gap-4">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt, lorem id volutpat finibus, nibh orci volutpat ipsum, 
+          sed dignissim sapien lectus sit amet turpis. Quisque a orci bibendum, euismod dolor a, fringilla lacus.
+        </p>
+
+        <p>
+          In non neque neque. Proin et eros in libero luctus cursus. Nam eleifend facilisis nunc, ut semper risus ultrices ac. Duis lobortis vel sem
+          id facilisis. Proin eget lectus nunc. Morbi viverra sed mi eu varius. 
+        </p>
+      </div>
+    </section>
+
     </>
   );
 }
---
+++ src/components/layout/SectionHeaders.js
@@ -0,0 +1,13 @@
+export default function SectionHeaders({subHeader, mainHeader}) {
+    return (
+        <>
+            <h3 className="uppercase font-bold leading-4">
+                {subHeader}
+            </h3>
+
+            <h2 className="font-bold text-yellow text-4xl italic">
+                {mainHeader}
+            </h2>
+        </>
+    );
+}
