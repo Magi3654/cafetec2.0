f2f7f47c126814dbee254f746c81f628e92c8988
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-20 16:25:25 -0700

    change stroke icons

--- src/components/icons/ChevronRight.js
+++ src/components/icons/ChevronRight.js
@@ -1,7 +1,7 @@
 export default function ChevronRight({className = "w-5 h-4"}) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={className}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={className}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
     )
 }
