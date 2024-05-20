af4a7cb7e4ac0bfead09c6ea9267dcc152b5d9f9
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-19 23:11:25 -0700

    margin content navbar

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,31 +1,30 @@
 'use client'
 import { CartContext, cartProductPrice } from "@/components/AppContext";
 import SectionHeaders from "@/components/layout/SectionHeaders";
-import { use, useContext, useEffect, useState } from "react";
-import Image from "next/image";
-import Trash from "@/components/icons/Trash";
 import AddressInputs from "@/components/layout/AddressInputs";
+import { useContext, useEffect, useState } from "react";
 import { UseProfile } from "@/components/UseProfile";
+import Trash from "@/components/icons/Trash";
+import Image from "next/image";
 
 export default function CartPage() {
-    const {cartProducts, removeCartProduct} = useContext(CartContext)
-    const [address, setAddress]= useState({});
+    const {cartProducts, removeCartProduct} = useContext(CartContext);
+    const [address, setAddress] = useState({});
     const {data:profileData} = UseProfile();
 
-    useEffect(()=>{
-        if(profileData?.city){
-            const {phone, streetAddress, city, postalCode, country}= profileData
-            const addressFromProfile ={
+    useEffect(() => {
+        if(profileData?.city) {
+            const {phone, streetAddress, city, postalCode, country} = profileData;
+            const addressFromProfile = {
                 phone,
                 streetAddress,
                 city,
                 postalCode,
                 country
             }
-
             setAddress(addressFromProfile);
         }
-    },)
+    }, [profileData]);
 
     let total = 0 
 
@@ -44,59 +43,55 @@ export default function CartPage() {
             </div>
             <div className=" mt-8 grid gap-8 grid-cols-2">
                 <div>
-                    {
-                        cartProducts?.length === 0 && (
-                            <div>
-                                Carrito vacio
-                            </div>
-                        )
-                    }
-                    {
-                        cartProducts?.length > 0 && cartProducts.map((product, index) => (
-                            <div className="flex items-center gap-4 mb-2 border-b py-4">
-                                <div className="w-24">
-                                    <Image width={240} height={240} src={product.image} alt={''}/>
-                                </div>
-                                <div className="grow">
-                                    <h3 className="font-semibold">
-                                        {product.name}
-                                    </h3>
-                                    {product.size && (
-                                        <div className="text-sm">
-                                            Tamaño: <span>{product.size.name}</span>
-                                        </div>
-                                    )}
-                                    {product.extras?.length >0 && (
-                                        <div className="text-sm text-semiGray">
-                                            Extras: {product.extras.map(extra=>(
-                                                <div>{extra.name} ${extra.price}</div>
-                                            ))}
-                                        </div>
-                                    )}
+                    {cartProducts?.length === 0 && (
+                        <div>Carrito vacio</div>
+                    )}
 
-                                </div>
-                                <div className="text-lg font-semibold">
-                                        ${cartProductPrice(product)}
-                                </div>
-                                <div className="ml-2">
-                                    <button 
+                    {cartProducts?.length > 0 && cartProducts.map((product, index) => (
+                        <div className="flex items-center gap-4 mb-2 border-b py-4">
+                            <div className="w-24">
+                                <Image width={240} height={240} src={product.image} alt={''}/>
+                            </div>
+                            <div className="grow">
+                                <h3 className="font-semibold">{product.name}</h3>
+                                    
+                                {product.size && (
+                                    <div className="text-sm">
+                                        Tamaño: <span>{product.size.name}</span>
+                                    </div>
+                                )}
+
+                                {product.extras?.length >0 && (
+                                    <div className="text-sm text-semiGray">
+                                        Extras: {product.extras.map(extra => (
+                                            <div>{extra.name} ${extra.price}</div>
+                                        ))}
+                                    </div>
+                                )}
+
+                            </div>
+                                
+                            <div className="text-lg font-semibold">
+                                ${cartProductPrice(product)}
+                            </div>
+                            
+                            <div className="ml-2">
+                                <button 
                                     type = "button"
-                                    onClick={()=> removeCartProduct(index)}
+                                    onClick={() => removeCartProduct(index)}
                                     className="p-2">
-                                        <Trash/>
-                                    </button>
-                                </div>
+                                    <Trash/>
+                                </button>
                             </div>
-                        ))
-                    }
-                    <div className="py-2 text-right pr-16">
-                        <span className="textsemiGray">
+                        </div>
+                    ))}
 
-                        Total:
-                        </span>
-                        <span className=" text-lg font-semibold pl-2">$ {total} </span> 
+                    <div className="py-2 text-right pr-16">
+                        <span className="textsemiGray">Total:</span>
+                        <span className="text-lg font-semibold pl-2">$ {total} </span> 
                     </div>
                 </div>
+
                 <div className="bg-gray p-4 rounded-lg">
                     <h2>Pagar Pedido</h2>
                     <form>
--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
-        <main className="lg:max-w-4xl mx-auto p-4"> 
+        <main className="mx-auto p-4 lg:max-w-4xl pb-[var(--navbar-height)]"> 
           <AppProvider>
             <Toaster></Toaster>
             <Header/>
