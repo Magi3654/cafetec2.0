80d31d1f8f3e3a5b24627564201f641226094fe5
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-20 03:49:01 -0700

    Add icons navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,9 @@
 'use client'
-import { CartContext } from "@/components/AppContext"
+import { CartContext } from "@/components/AppContext";
+import Money from "@/components/icons/Money";
+import Home from "@/components/icons/Home"
+import Store from "@/components/icons/Store"
+import User from "@/components/icons/User";
 import { useSession } from "next-auth/react";
 import { useContext } from "react";
 import Link from "next/link";
@@ -20,10 +24,25 @@ export default function Navbar() {
 
     return (
         <nav className="flex items-center justify-around bg-white font-medium py-4 fixed bottom-0 rounded rounded-se-2xl rounded-ss-2xl left-0 right-0 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-            <Link className='mx-2' href={'/'}>Home</Link>
-            <Link className='mx-2' href={'/menu'}>Menu</Link>
-            <Link className='mx-2' href={'/payment'}>Método de Pago</Link>
-            <Link className='mx-2' href={'/#about'}>About</Link>
+            <div className="flex flex-col mx-2 items-center">
+                <Home/>
+                <Link href={'/'}>Home</Link>
+            </div>
+
+            <div className="flex flex-col mx-2 items-center">
+                <Store />
+                <Link href={'/menu'}>Menu</Link>
+            </div>
+
+            <div className="flex flex-col mx-2 items-center">
+                <Money/>
+                <Link href={'/payment'}>Tarjetas</Link>
+            </div>
+
+            <div className="flex flex-col mx-2 items-center">
+                <User />
+                <Link href={'/profile'}>Perfil</Link>
+            </div>
         </nav>
     )
 }
